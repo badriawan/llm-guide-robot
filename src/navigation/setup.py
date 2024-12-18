30ba0e1543c6c762d2acf4fa7aef478d8b971a91
badriawan <yusufbadriawan@gmail.com>
2024-12-18 17:32:19 +0700

    updated 18 Dec

---
+++ src/navigation/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'map'), glob('map/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'nav_to_pose = navigation.nav_to_pose:main',
+        'nav_to_pose2 = navigation.nav_to_pose2:main',
+
+        ],
+    },
+)
+
